ae80894ef1d61caa28f8d1628cfcef176749374e
Prasad Lokhande <Prasad.Lokhande@xoriant.com>
2025-10-29 17:13:53 +0530

    frontend seprated

--- CleanArchitecture/ProductManagement.Api/ProductManagement.Application/ResponseModel/ProductResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProductManagement.Application.ResponseModel
-{
-    public class ProductResponse
-    {
-        public bool Success { get; set; }
-        public string Message { get; set; }
-        public object Data { get; set; }
-    }
-}
